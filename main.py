cc9820f418aefd41c213a65a942394b39310b63c
Matias Espinosa <matias.espinosa@uc.cl>
2023-04-12 14:37:43 -0400

    Changed and added colors in main.py file

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 if __name__ == '__main__':
-    print('¡Bienvenido a Chequeador de Paridad! (^o^)')
+    print('\033[1;32;40m¡Bienvenido a Chequeador de Paridad! (^o^)\033[0m')
     user_input = input(
         'Ingrese un \033[1;34;40mnúmero entero positivo\033[0m: ')
     if user_input.isnumeric():
         user_number = int(user_input)
-        parity = '\033[1;34;40mPar\033[0m' if user_number % 2 == 0 else '\033[1;34;40mImpar\033[0m'
+        parity = '\033[1;36;40mPar\033[0m' if user_number % 2 == 0 else '\033[1;33;40mImpar\033[0m'
         print(
             f'El número {user_number} es {parity}\n¡Gracias por usar el programa! ^^')
     else:
